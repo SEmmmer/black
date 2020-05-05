877709f97be33760dcfb583ff03e2c86959c82c9
Shirakami Emmmer <yjhdandan@163.com>
2020-05-05 16:02:31 +0800

    success

--- api_killer.py
+++ api_killer.py
@@ -4,5 +4,13 @@ import bilibili_api.user
 from bilibili_api import Verify
 from bilibili_api.user import UserOperate
 
-print(sec.file["CSRF"])
-verify = Verify(sessdata="your sessdata", csrf="your csrf")
+verify = Verify(sessdata=sec.file["SESSDATA"], csrf=sec.file["CSRF"])
+# 请务必不要泄漏这两项数据
+file = open("black.txt", "r")
+
+for line in file:
+    line = line.split()
+    uid = int(line[0])
+    user = UserOperate(uid=uid, verify=verify)
+    user.set_black()
+    print("目标", str(uid), "已经被清除")
