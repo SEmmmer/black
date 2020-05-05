1991c5250de15e089c8bb8c66789166ef1de348b
Shirakami Emmmer <yjhdandan@163.com>
2020-05-05 12:37:48 +0800

    add fun

--- find_danmaku.py
+++ find_danmaku.py
@@ -2,17 +2,26 @@
 import re
 import os
 
-for line in open("file.txt", "r"):
-    print("1", line)
+black = []
+white = []
+for line in open("aaa.txt", "r"):
+    try:
+        array = line.split(":", 2)
+        if (array[1] not in black) and (array[1] not in white):
+            print("请判断该弹幕是否为引战弹幕，是请输入1，不是请直接回车")
+            print(array[2])
+            string = input()
+            if string == "1":
+                black.append(array[1])
+            else:
+                white.append(array[1])
+        else:
+            continue
+    except KeyboardInterrupt:
+        black_file = open("black.txt", "w")
+        for uid in black:
+            black_file.writelines(uid)
 
 
-
-
-
-#
-# try:
-#
-# except KeyboardInterrupt:
-#     print("KeyboardInterrupt")
-# else:
-#     print("over")
+    else:
+        print("over")
