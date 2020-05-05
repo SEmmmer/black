476cb0c8206a004af4258b5a1582ee2090d08396
Shirakami Emmmer <yjhdandan@163.com>
2020-05-05 14:56:14 +0800

    update api

--- api_killer.py
+++ api_killer.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*
-import bilibili_api
-from bilibili_api import user
+import bilibili_api.user
+from bilibili_api import Verify
+from bilibili_api.user import UserOperate
+
+verify = Verify(sessdata="your sessdata", csrf="your csrf")
