714195cd4be2db12f2330fe0cf74fe73d86802b7
Shirakami Emmmer <yjhdandan@163.com>
2020-05-06 11:47:37 +0800

    update

--- api_block_tool.py
+++ api_block_tool.py
@@ -1,11 +1,24 @@
-def ban_user(user, room_id: int, uid: int, hour: int):
-    url = '//api.live.bilibili.com/banned_service/v2/Silent/add_block_user'
-    data = {
-        'roomid': room_id,
-        'block_uid': uid,
-        'hour': hour,
-        'csrf_token': ,
-        'csrf': ,
-    }
-    json_rsp = await user.other_session.request_json('POST', url, data=data, headers=user.dict_bili['pcheaders'])
-    return json_rsp
+# -*- coding: utf-8 -*
+import me
+import json
+import requests
+
+url = "http://api.live.bilibili.com/liveact/ajaxGetBlockList"
+url2 = "http://api.live.bilibili.com/banned_service/v2/Silent/add_block_user"
+
+cookies = me.cookie
+
+requestData = {
+
+    "roomid": 763869,
+    "block_uid": 544906812,
+    "page": 1,
+    "hour": 70,
+    "csrf": me.user["CSRF"],
+    "csrf_token": me.user["CSRF"],
+    "visit_id": 0
+}
+
+code = requests.post(url, data=json.dumps(requestData), cookies=cookies)
+text = json.loads(code.text)
+print(text)
