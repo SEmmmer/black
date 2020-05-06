5f46481258e4279f667f983cdd9f58150eaa4a67
Shirakami Emmmer <yjhdandan@163.com>
2020-05-06 22:49:25 +0800

    update

--- api_block_tool.py
+++ api_block_tool.py
@@ -11,12 +11,12 @@ cookies = me.cookie
 requestData = {
 
     "roomid": 763869,
-    "block_uid": 544906812,
-    "page": 1,
-    "hour": 70,
-    "csrf": me.user["CSRF"],
-    "csrf_token": me.user["CSRF"],
-    "visit_id": 0
+    # "block_uid": 544906812,
+    "page": 1
+    # "hour": 70,
+    # "csrf": me.user["CSRF"],
+    # "csrf_token": me.user["CSRF"],
+    # "visit_id": 0
 }
 
 code = requests.post(url, data=json.dumps(requestData), cookies=cookies)
