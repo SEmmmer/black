016e85460080407fe9c268dde2191625d95cc00c
Shirakami Emmmer <yjhdandan@163.com>
2020-05-07 23:09:37 +0800

    success

--- api_block_tool.py
+++ api_block_tool.py
@@ -12,8 +12,10 @@ block_man = return_message["data"][0]["uid"]
 
 print(block_man)
 
-block_url = "https://api.live.bilibili.com/banned_service/v2/Silent/add_block_user" + "?roomid=763869" + f"&block_uid={block_man}" + "&hour=720" + f"&csrf={me.user['CSRF']}" + f"&csrf_token={me.user['CSRF']}"
-print(block_url)
-block_it = requests.post(block_url, cookies=cookies)
+block_url = "https://api.live.bilibili.com/banned_service/v2/Silent/add_block_user"
+headers = {"content-type": "application/x-www-form-urlencoded"}
+data = f"roomid=763869&block_uid=544906812&hour=1&csrf_token={me.user['CSRF']}&csrf={me.user['CSRF']}&visit_id="
+
+block_it = requests.post(block_url, data=data, cookies=cookies, headers=headers)
 return_message = json.loads(block_it.text)
 print(return_message)
