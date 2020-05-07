1baac56144edd561cfe7bb16b6571f1a55f9221d
Shirakami Emmmer <yjhdandan@163.com>
2020-05-07 16:52:53 +0800

    success

--- api_block_tool.py
+++ api_block_tool.py
@@ -6,11 +6,14 @@ import requests
 block_list = "http://api.live.bilibili.com/liveact/ajaxGetBlockList" + "?roomid=763869" + "&page=1" + f"&csrf={me.user['CSRF']}" + f"&csrf_token={me.user['CSRF']}"
 cookies = me.cookie
 
-code = requests.get(block_list, cookies=cookies)
-text = json.loads(code.text)
-block_man = text["data"][0]["uid"]
+get_list = requests.get(block_list, cookies=cookies)
+return_message = json.loads(get_list.text)
+block_man = return_message["data"][0]["uid"]
 
 print(block_man)
 
-url2 = "https://api.live.bilibili.com/banned_service/v2/Silent/add_block_user" + "?roomid=763869" + f"&block_uid={block_man} " + "&hour=720" + f"&csrf={me.user['CSRF']}" + f"&csrf_token={me.user['CSRF']}"
-
+block_url = "https://api.live.bilibili.com/banned_service/v2/Silent/add_block_user" + "?roomid=763869" + f"&block_uid={block_man}" + "&hour=720" + f"&csrf={me.user['CSRF']}" + f"&csrf_token={me.user['CSRF']}"
+print(block_url)
+block_it = requests.post(block_url, cookies=cookies)
+return_message = json.loads(block_it.text)
+print(return_message)
