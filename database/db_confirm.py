31a30f2d9a6df80485207e2d541172acec1f9362
Shirakami Emmmer <yjhdandan@163.com>
2020-05-15 23:14:57 +0800

    fix insert bug

--- database/db_confirm.py
+++ database/db_confirm.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*
 import pymongo
-import confirm
+
+
+def c_out():
+    global line
+    for man in black:
+        file.writelines(str(man) + "\n")
+        danmaku_file = open("file.txt", "r")
+        for line in danmaku_file:
+            array = line.split(":", 2)
+            if int(array[1]) == int(man):
+                puts = array[2].split()[0]
+                file.writelines(puts + "\n")
+        print("")
+        file.writelines("\n")
+        danmaku_file.close()
+
 
 black = []
 client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -10,9 +25,8 @@ uid_col = data_base.get_collection('uid')
 uid_list = uid_col.find({'type': "black"})
 for uid in uid_list:
     black.append(uid['uid'])
-print(black)
 
 file = open("confirm_list.txt", "a")
-confirm.c_out()
+c_out()
 print("over")
 file.close()
--- database/db_judge.py
+++ database/db_judge.py
@@ -3,28 +3,35 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 data_base = client['test']
+question = data_base['question']
+global regret_uid
 
 try:
     while True:
-        question = data_base.get_collection('question')
-        uid_col = data_base.get_collection('uid')
+        uid_col = data_base['uid']
         uid_list = uid_col.find()
-        single = question.find_one()
+        single = question.aggregate([{'$sample': {'size': 1}}])
+        for doc in single:
+            single = doc
+
+        # 每循环一次，更新一次列表
         if single['uid'] not in uid_list:
+            # 趁另一个用户不注意提前插入文本
+            # 防止两个用户同时改到一道题
+            uid_col.insert_one({"uid": single['uid'], "type": "white"})
+            regret_uid = single['uid']
+            print(regret_uid)
             print('请问该言论是否引战，是请输入1，否请直接回车：')
             print(single['danmaku'])
             string = input()
 
             if string == '1':
-                doc = {"uid": single['uid'], "type": "black"}
-                uid_col.insert_one(doc)
-            else:
-                doc = {"uid": single['uid'], "type": "white"}
-                uid_col.insert_one(doc)
-
-        question.delete_one(single)
+                # 先 斩 后 奏
+                uid_col.update_one({"uid": single['uid']}, {"$set": {"type": "black"}}, True)
+        question.delete_many({"danmaku": single['danmaku']})
         # 处理完该问题
         # 从弹幕库中删除
 
 except KeyboardInterrupt:
     print("用户终止进程")
+    data_base['uid'].delete_many({'uid': regret_uid})
