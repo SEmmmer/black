869a99da37ff9577bfb197926db5b1cf1c41d6f5
Shirakami Emmmer <yjhdandan@163.com>
2020-05-16 13:46:16 +0800

    fix delete bug

--- database/db_judge.py
+++ database/db_judge.py
@@ -9,26 +9,25 @@ global regret_uid
 try:
     while True:
         uid_col = data_base['uid']
-        uid_list = uid_col.find()
         single = question.aggregate([{'$sample': {'size': 1}}])
         for doc in single:
             single = doc
 
-        # 每循环一次，更新一次列表
-        if single['uid'] not in uid_list:
+            # 每循环一次，更新一次列表
             # 趁另一个用户不注意提前插入文本
             # 防止两个用户同时改到一道题
-            uid_col.insert_one({"uid": single['uid'], "type": "white"})
-            regret_uid = single['uid']
-            print(regret_uid)
-            print('请问该言论是否引战，是请输入1，否请直接回车：')
-            print(single['danmaku'])
-            string = input()
+        uid_col.insert_one({"uid": single['uid'], "type": "white"})
+        regret_uid = single['uid']
+        print(regret_uid)
+        print('请问该言论是否引战，是请输入1，否请直接回车：')
+        print(single['danmaku'])
+        string = input()
 
-            if string == '1':
-                # 先 斩 后 奏
-                uid_col.update_one({"uid": single['uid']}, {"$set": {"type": "black"}}, True)
+        if string == '1':
+            # 先 斩 后 奏
+            uid_col.update_one({"uid": single['uid']}, {"$set": {"type": "black"}}, True)
         question.delete_many({"danmaku": single['danmaku']})
+        question.delete_many({'uid': single['uid']})
         # 处理完该问题
         # 从弹幕库中删除
 
