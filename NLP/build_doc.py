c700a72637ac15cb68575b4f354a2e58b3e69974
Shirakami Emmmer <yjhdandan@163.com>
2020-05-27 17:22:19 +0800

    add train_moudle.py

---
+++ NLP/build_doc.py
@@ -0,0 +1,33 @@
+import os
+import jieba.analyse
+
+PATH = "/Users/yangjinghua/bilibili-vtuber-danmaku/15536/"
+
+jieba.analyse.set_stop_words("NLP/stopwords")
+jieba.load_userdict("NLP/userdict.txt")
+sum_file = open("sum_file.txt", "w")
+
+for n in range(2):
+    year = 2019 + n
+    for y in range(12):
+        month = 1 + y
+        for t in range(31):
+            day = 1 + t
+
+            date = [str(year), str(month), str(day)]
+            filename = "-".join(date) + ".txt"
+            if os.path.exists(PATH + filename):
+                print(filename + " exist")
+                file = open(PATH + filename, "r")
+                for line in file:
+                    if line[0] != "1":
+                        continue
+                    danmaku = line.split(":", 2)[2]
+                    seg_list = jieba.analyse.extract_tags(danmaku)
+                    output = " ".join(seg_list) + " "
+                    print(output)
+                    sum_file.writelines(output)
+                file.close()
+        day = 1
+    month = 1
+sum_file.close()
--- NLP/words_jieba.py
+++ NLP/words_jieba.py
@@ -4,28 +4,24 @@ from nltk.stem.porter import *
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 file = open("file2.txt", "r")
-file_two = open("new.txt", "w")
+file_two = open("new1.txt", "w")
 jieba.load_userdict("userdict.txt")
 
-# jieba.analyse.set_stop_words('stopwords')
-#
-# stemmer = PorterStemmer()
-# vector = TfidfVectorizer(smooth_idf=False)
+jieba.analyse.set_stop_words('stopwords')
+
+stemmer = PorterStemmer()
+vector = TfidfVectorizer(smooth_idf=False)
 for line in file:
     danmaku = line.split(":", 2)[2]
-    # seg_list = jieba.analyse.extract_tags(danmaku, 20)
+    seg_list = jieba.analyse.extract_tags(danmaku, 20)
     # seg_list = [stemmer.stem(word) for word in seg_list]
-    seg_list = jieba.cut(danmaku)
-    # if len(seg_list) < 2:
-    #     continue
-    # print(seg_list)
+    # seg_list = jieba.cut(danmaku)
+    if len(seg_list) < 2:
+        continue
     # X = vector.fit_transform(seg_list)
-    # print(X.toarray())
-    # print(vector.get_feature_names())
-    result = "/".join(seg_list)
+    result = "/".join(seg_list) + "\n"
 
     if '/' in result:
         file_two.writelines(result)
 file_two.close()
 file.close()
-# file_thr.close()
