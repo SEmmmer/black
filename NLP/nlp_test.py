0ea44c1235bcb4a17c519d646567ba7a72be8598
Shirakami Emmmer <yjhdandan@163.com>
2020-05-30 08:56:49 +0800

    add dic

--- NLP/nlp_test.py
+++
@@ -1,8 +0,0 @@
-file = open("NLP/userdict.txt", "r")
-names = []
-for line in file:
-    line = line.replace("\n", "").lower()
-    if line not in names:
-        names.append(line)
-for one in names:
-    print(one)
